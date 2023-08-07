20a40aabf1f4b2222a1de35479a3d3724e5e080f
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-08-07 18:44:09 -0300

    Create DB connection and Readme update

--- models/Server.js
+++ models/Server.js
@@ -1,16 +1,35 @@
+/* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
+import database from '../database/connection.js';
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 4000;
 
+    // Database connection
+    this.dbConnection();
+
     // Middelwares
     this.middelwares();
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  async dbConnection() {
+    try {
+      await database.authenticate();
+      // await database.sync();
+      // to drop all the tables and create again
+      // await database.sync({ force: true });
+      console.log('Connection to the database was established successfully.');
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   middelwares() {
+    // Cors
     this.app.use(cors());
   }
 
