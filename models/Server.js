f90aea95b0d11d389d2c7084f085142d7720b773
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-08-11 17:58:10 -0300

    Create error handler middleware

--- models/Server.js
+++ models/Server.js
@@ -3,23 +3,33 @@ import express from 'express';
 import cors from 'cors';
 import database from '../database/connection.js';
 
+import errorHandlerMiddleware from '../middlewares/error-handler.js';
+import routerUsers from '../routes/user.routes.js';
+
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 4000;
+    this.usersRoute = '/user';
 
     // Database connection
     this.dbConnection();
 
     // Middelwares
     this.middelwares();
+
+    // Routes
+    this.routes();
+
+    // Error Handler
+    this.app.use(errorHandlerMiddleware);
   }
 
   // eslint-disable-next-line class-methods-use-this
   async dbConnection() {
     try {
-      await database.authenticate();
-      // await database.sync();
+      // await database.authenticate();
+      await database.sync();
       // to drop all the tables and create again
       // await database.sync({ force: true });
       console.log('Connection to the database was established successfully.');
@@ -31,6 +41,10 @@ class Server {
   middelwares() {
     // Cors
     this.app.use(cors());
+  }
+
+  routes() {
+    this.app.use(this.usersRoute, routerUsers);
   }
 
   listen() {
