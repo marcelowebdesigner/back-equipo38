e18645f36dfe0183bc653c5c2463c8d1bc7cf281
marcelo <marcelocamusso@hotmail.com>
2023-08-23 20:29:06 +0200

    feature:validatorJwt: Create validate jwt.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,36 @@
-import { loginUser } from '../services/auth.service.js'; // Cambio aquí
+import { loginUser } from '../services/auth.service.js';
+import generateAccessToken from '../helpers/generatorJwt.js'; // Import the function without curly braces
+import { validateAccessToken } from '../helpers/validatorJwt.js';
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    const user = await loginUser(email, password); // Cambio aquí
+    const user = await loginUser(email, password);
 
-    res.status(200).json({ user });
+    if (user) {
+      // If login was successful and you have a user
+
+      // Generate the token after successful authentication
+      const token = generateAccessToken(user.id); // Assuming user.id is the user's unique identifier
+
+      const decodedToken = validateAccessToken(token);
+
+      if (decodedToken) {
+        // The token is valid
+        console.log('Valid token. User information:', decodedToken);
+        // ... Additional logic after successful authentication
+        res.status(200).json({ message: 'Authentication successful', token });
+      } else {
+        // The token is not valid
+        console.log('Invalid token.');
+        res.status(401).json({ message: 'Authentication failed' });
+      }
+
+    } else {
+      // If login failed
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
   } catch (error) {
     next(error);
   }
---
+++ helpers/validatorJwt.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+// Function to validate JWT token
+export function validateAccessToken(token) {
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    return decoded;
+  } catch (error) {
+    // Token validation failed
+    return null;
+  }
+}
+
