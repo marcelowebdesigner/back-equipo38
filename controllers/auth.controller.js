84a9d70463d88cf9172c6afbb4431f6a37ae6d69
marcelo <marcelocamusso@hotmail.com>
2023-08-30 19:45:48 +0200

    feat:generateJwt:Refactor.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,32 +1,17 @@
 import { loginUser } from '../services/auth.service.js';
-import generateAccessToken from '../helpers/generatorJwt.js'; // Import the function without curly braces
-import { validateAccessToken } from '../helpers/validatorJwt.js';
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    const user = await loginUser(email, password);
+    const { user, token } = await loginUser(email, password);
 
     if (user) {
       // If login was successful and you have a user
+      console.log('Valid token. User information:', user);
 
-      // Generate the token after successful authentication
-      const token = generateAccessToken(user.id); // Assuming user.id is the user's unique identifier
-
-      const decodedToken = validateAccessToken(token);
-
-      if (decodedToken) {
-        // The token is valid
-        console.log('Valid token. User information:', decodedToken);
-        // ... Additional logic after successful authentication
-        res.status(200).json({ message: 'Authentication successful', token });
-      } else {
-        // The token is not valid
-        console.log('Invalid token.');
-        res.status(401).json({ message: 'Authentication failed' });
-      }
-
+      // ... Additional logic after successful authentication
+      res.status(200).json({ message: 'Authentication successful', token });
     } else {
       // If login failed
       res.status(401).json({ message: 'Invalid credentials' });
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,39 +1,42 @@
-
 import bcryptjs from 'bcryptjs';
 import User from '../models/User.js';
 import generateAccessToken from '../helpers/generatorJwt.js';
 
 export const loginUser = async (email, password) => {
-  // Check if the user is registered
-  const user = await User.findOne({
-    where: {
-      us_email: email,
-    },
-  });
+  try {
+    // Check if the user is registered
+    const user = await User.findOne({
+      where: {
+        us_email: email,
+      },
+    });
 
-  if (!user) {
-    throw new Error(
-      `There are no users with the email ${email} on the database. Please register or check the email address.`,
-    );
+    if (!user) {
+      throw new Error(
+        `There are no users with the email ${email} on the database. Please register or check the email address.`,
+      );
+    }
+
+    // Check if the user is active
+    if (!user.us_active) {
+      throw new Error('The user is inactive. Please contact an administrator.');
+    }
+
+    // Validate encrypted password
+    const isPasswordValid = bcryptjs.compareSync(password, user.us_password);
+
+    if (!isPasswordValid) {
+      throw new Error('Incorrect password.');
+    }
+
+    // Generate jwt token using previously defined function
+    const accessToken = generateAccessToken(user.id);
+
+    return {
+      user,     // Make sure "user" is defined here
+      token: accessToken,
+    };
+  } catch (error) {
+    throw error; // Re-throw the error to be caught in the controller
   }
-
-  // Check if the user is active
-  if (!user.us_active) {
-    throw new Error('The user is inactive. Please contact an administrator.');
-  }
-
-  // Validate encripted password 
-  const isPasswordValid = bcryptjs.compareSync(password, user.us_password);
-
-  if (!isPasswordValid) {
-    throw new Error('Incorrect password.');
-  }
-
-  // Generate jwt token using previously defined function
-  const accessToken = generateAccessToken(user.id);
-
-  return {
-    user,
-    token: accessToken,
-  };
 };
