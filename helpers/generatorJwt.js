6fea5fda77d9762bef363b963c38343d91493d23
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-09-11 09:41:43 -0300

    JWT generator and validator created with payload

--- helpers/generatorJwt.js
+++ helpers/generatorJwt.js
@@ -1,6 +1,24 @@
 import jwt from 'jsonwebtoken';
 
 // function to generate the jwt token
-export default function generateAccessToken(userId) {
-    return jwt.sign({ userId }, process.env.SECRET, { expiresIn: '15m' });
-  }
+export default function generateAccessToken(id) {
+  return new Promise((resolve, reject) => {
+    const payload = { id };
+    // console.log(payload);
+
+    jwt.sign(
+      payload,
+      process.env.SECRETKEY,
+      {
+        expiresIn: '15m',
+      },
+      (error, token) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(token);
+        }
+      },
+    );
+  });
+}
