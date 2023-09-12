e06ea6860c470457ff66e887ee1f80b507dd0882
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-09-12 11:54:23 -0300

    change how user information is sent

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,7 +6,7 @@ const login = async (req, res, next) => {
   try {
     const user = await loginUser(email, password);
 
-    res.status(200).json({ user });
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
